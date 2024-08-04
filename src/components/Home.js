5a1ea62fb6b2cbb72cac98b061f9274f6ad073bc
Carlos Esteve <charlsef66@gmail.com>
2024-08-04 16:04:18 +0200

    url-redes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,14 +4,20 @@ import './Home.css';
 const Home = () => {
   return (
     <section id="home" class="home">
-        <div class="contenido-banner">
-            <div class="contenedor-img">
-               <img src="assets/img/hero.png" alt=""></img>
-            </div>
-            <h1>CARLOS ESTEVE</h1>
-            <h2>Ingeniero de Software - Experto UI/UX</h2>
+    <div class="contenido-banner">
+        <div class="contenedor-img">
+            <img src="img/hero.png" alt=""></img>
         </div>
-    </section>
+        <h1>CARLOS ESTEVE</h1>
+        <h2>Ingeniero de Software - Experto UI/UX</h2>
+        <div class="redes">
+            <a href="https://www.instagram.com/carlosesteve23/"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://x.com/Charlsef23"><i class="fa-brands fa-x"></i></a>
+            <a href="https://tiktok.com/charlsef23"><i class="fa-brands fa-tiktok"></i></a>
+            <a href="www.linkedin.com/in/carlosestevefernández"><i class="fa-brands fa-linkedin-in"></i></a>
+        </div>
+    </div>
+</section>
   );
 };
 
