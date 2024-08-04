64bc8eda602d153aac168ed9181d1510c754b7f9
Carlos Esteve <charlsef66@gmail.com>
2024-08-04 09:01:12 +0200

    0

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,28 @@ import React from 'react';
 const NavBar = ({ toggleDarkMode }) => {
   return (
     <nav>
-      <h1>Mi Portafolio</h1>
-      <button onClick={toggleDarkMode}>Toggle Theme</button>
-    </nav>
+    <div class="contenedor-header">
+        <header>
+            <div class="logo">
+                <a href="#">Carlos</a>
+            </div>
+            <nav id="nav">
+                <ul>
+                    <li><a href="#home" onclick="seleccionar()">HOME</a></li>
+                    <li><a href="#about" onclick="seleccionar()">ABOUT</a></li>
+                    <li><a href="#skills" onclick="seleccionar()">SKILLS</a></li>
+                    <li><a href="#curriculum" onclick="seleccionar()">CURRICULUM</a></li>
+                    <li><a href="#portfolio" onclick="seleccionar()">PORTFOLIO</a></li>
+                    <li><a href="#contacto" onclick="seleccionar()">CONTACTO</a></li>
+                </ul>
+                <button id="theme-toggle" onclick="toggleDarkMode()">Toggle Theme</button>
+            </nav>
+            <div class="nav-responsive" onclick="mostrarOcultarMenu()">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+        </header>
+    </div>
+</nav>
   );
 };
 
---
+++ src/components/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section className="skills">
+      <h2>Contacto</h2>
+      <p>Formulario de contacto o información de contacto.</p>
+    </section>
+  );
+};
+
+export default Skills;
